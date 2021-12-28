51f38905a66f1ca6d981aaa80717935108dc55bf
Aman Mavai <amanmavai@gmail.com>
2021-12-28 10:20:43 +0530

    update remix config for mdx handling

--- remix.config.js
+++ remix.config.js
@@ -1,3 +1,16 @@
+// can be an sync / async function or an object
+const handleMdx = async (filename) => {
+  const [rehypeHighlight, remarkToc] = await Promise.all([
+    import("rehype-highlight").then((mod) => mod.default),
+    import("remark-toc").then((mod) => mod.default),
+  ]);
+
+  return {
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [rehypeHighlight],
+  };
+};
+
 /**
  * @type {import('@remix-run/dev/config').AppConfig}
  */
@@ -6,5 +19,6 @@ module.exports = {
   assetsBuildDirectory: "public/build",
   publicPath: "/build/",
   serverBuildDirectory: "api/_build",
-  ignoredRouteFiles: [".*"]
+  ignoredRouteFiles: [".*"],
+  mdx: handleMdx,
 };
